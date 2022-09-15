9a140b8ad9b0975d4f7bf189f206f2838b3ea7f2
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-15 19:14:40 -0400

    good

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
- 
+        double creditBalance = 5000;
+        double firstMonth = creditBalance + creditBalance * 0.17;
+        double secondMonth = firstMonth + firstMonth * 0.17;
+
+        System.out.println("The first month balance is " + firstMonth);
+        System.out.println("The second month balance is " + secondMonth);
+
     }
 }
